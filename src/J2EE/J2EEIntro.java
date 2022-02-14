2524038cd0881346c47866ab450e49310e1ed2e2
daivik <dj1pandya@gmail.com>
2022-02-14 15:01:09 +0530

    Adding new File

---
+++ src/J2EE/J2EEIntro.java
@@ -0,0 +1,25 @@
+package J2EE;
+
+import java.util.Arrays;
+
+public class J2EEIntro {
+//    public static void main(String[] args){
+//    }
+
+//    What is J2EE
+//    Short for Java 2 Platform Enterprise Edition.
+//    J2EE is a platform-independent, Java-centric environment from Sun for developing, building and deploying Web-based enterprise applications online.
+//    The J2EE platform consists of a set of services, APIs, and protocols that provide the functionality for developing multi-tiered, Web-based applications.
+
+
+
+    //Need of J2EE
+/*
+    Some key advantages are:
+
+   1. It provides one of the key functionality of cross-platform portability.
+   2. Like any other Java-based technology, open-source libraries are available easily for the common developer.
+   3. Handling some big enterprise-level application for deploying on the server-side easily.
+   4. Always maintained and covered the maximum global W3C Standard by J2EE technologies.
+*/
+}
